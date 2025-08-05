64fa7c23c40f33910b9ab2b959132576c7978083
Ashparshp <ashparsh.connects@gmail.com>
2025-08-06 00:28:15 +0530

    feat: enhance GetAllOrders to include additional order details and update all sales page template

--- Web Development/05_Stripe/internal/models/models.go
+++ Web Development/05_Stripe/internal/models/models.go
@@ -327,7 +327,7 @@ func (m *DBModel) GetAllOrders() ([]Order, error) {
 
 	query := `
 		select 
-			o.id, o.widget_id, o.transaction_id, o.customer_id, o.status_id
+			o.id, o.widget_id, o.transaction_id, o.customer_id, o.status_id,
 			o.quantity, o.amount, o.created_at, o.updated_at,
 			w.id, w.name, t.id, t.amount, t.currency, t.last_four,
 			t.expiry_month, t.expiry_year, t.payment_intent, t.bank_return_code,
