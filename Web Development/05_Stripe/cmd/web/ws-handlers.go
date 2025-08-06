48f142bfb5960c83a07353316c2f085972ec4411
Ashparshp <ashparsh.connects@gmail.com>
2025-08-06 19:38:35 +0530

    feat: add WebSocket support for user interactions and account deletion notifications

---
+++ Web Development/05_Stripe/cmd/web/ws-handlers.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+type WSPayload struct {
+	Action      string              `json:"action"`
+	Message     string              `json:"message"`
+	UserName    string              `json:"username"`
+	MessageType string              `json:"message_type"`
+	Conn        WebSocketConnection `json:"-"`
+}
+
+type WSJsonResponse struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
+	UserID  string `json:"user_id"`
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// Allow all connections for simplicity, but you may want to restrict this in production
+		return true
+	},
+}
+
+var clients = make(map[WebSocketConnection]string)
+
+var wsChan = make(chan WSPayload)
+
+func (app *application) WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Could not upgrade connection", http.StatusInternalServerError)
+		return
+	}
+	defer ws.Close()
+
+	app.infoLog.Printf("Client connected from %s", r.RemoteAddr)
+
+	var response WSJsonResponse
+
+	response.Message = "Connected to server"
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		app.errorLog.Println("Error writing JSON response:", err)
+		return
+	}
+
+	conn := WebSocketConnection{Conn: ws}
+	clients[conn] = ""
+
+	go app.ListenForWs(&conn)
+
+}
+
+func (app *application) ListenForWs(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			app.errorLog.Println("Recovered in ListenForWs:", r)
+		}
+	}()
+
+	var payload WSPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// do nothing
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func (app *application) ListenToWsChannel() {
+	var response WSJsonResponse
+
+	for {
+		e := <-wsChan
+		switch e.Action {
+		case "deleteUser":
+			response.Action = "logout"
+			response.Message = "Your account has been deleted"
+			app.broadcastToAllClients(response)
+		default:
+		}
+	}
+}
+
+func (app *application) broadcastToAllClients(response WSJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			app.errorLog.Println("Error writing JSON to client:", err)
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
--- Web Development/05_Stripe/go.mod
+++ Web Development/05_Stripe/go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/alexedwards/scs/v2 v2.8.0 // indirect
 	github.com/bwmarrin/go-alone v0.0.0-20190806015146-742bb55d1631 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
 	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
 	golang.org/x/crypto v0.40.0 // indirect
