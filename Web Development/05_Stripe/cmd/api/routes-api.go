393eb5e5d4dfc508b8d17b3f0ce9f731ac309954
Ashparshp <ashparsh.connects@gmail.com>
2025-08-06 17:32:42 +0530

    feat: add endpoint and template for retrieving all users

--- Web Development/05_Stripe/cmd/api/routes-api.go
+++ Web Development/05_Stripe/cmd/api/routes-api.go
@@ -41,6 +41,7 @@ func (app *application) routes() http.Handler {
 		mux.Post("/get-sale-details/{id}", app.SaleDetails)
 		mux.Post("/refund", app.RefundCharge)
 		mux.Post("/cancel-subscription", app.CancelSubscription)
+		mux.Post("/all-users", app.AllUsers)
 	})
 
 	return mux
