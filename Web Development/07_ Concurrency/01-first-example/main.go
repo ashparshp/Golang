d37d865c5cabf6968ab593d43cf9593db8c56fa7
Ashparshp <ashparsh.connects@gmail.com>
2025-08-10 17:17:21 +0530

    fix: refactor printSomething function to accept WaitGroup for proper goroutine synchronization

--- Web Development/07_ Concurrency/01-first-example/main.go
+++ Web Development/07_ Concurrency/01-first-example/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done() // Ensure that Done is called when the goroutine finishes
 	fmt.Println(s)
 }
 
 func main() {
+
+	/* bad practice: using time.Sleep to wait for goroutines
 	go printSomething("This is the first thing to be printed!")
+	time.Sleep(1 * time.Second)
+	*/
 
-	/* bad practice: using time.Sleep to wait for goroutines */
-	// time.Sleep(1 * time.Second)
+	var wg sync.WaitGroup
+	words := []string{"alpha!", "beta!", "delta", "gamma!", "pi", "zeta!", "eta", "sigma!", "epsilon!"}
+	wg.Add(len(words)) // Set the number of goroutines to wait for
 
-	printSomething("This is the second thing to be printed!")
+	for i, word := range words {
+		go printSomething(fmt.Sprintf("Word %d: %s", i+1, word), &wg)
+	}
+	wg.Wait() // Wait for all goroutines to finish
+
+	wg.Add(1)
+	printSomething("This is the second thing to be printed!", &wg)
 }
